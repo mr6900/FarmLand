856ea634be8635b264ddc3149797e9e6727721ea
mr6900 <mr6900@students.inghamisd.org>
2019-05-21 16:06:09 -0400

    Added in all funtions for the main menu

---
+++ FarmLand/FarmLand/Game1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FarmLand
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        enum GameState
+        {
+            MainMenu,
+            Options,
+            Playing,
+        }
+        GameState CurrentState = GameState.MainMenu;
+
+        int screenWidth = 800, screenHeight = 600;
+
+
+        cButton plyBtn;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            graphics.PreferredBackBufferWidth = screenWidth;
+            graphics.PreferredBackBufferHeight = screenHeight;
+            //graphics.IsFullScreen = true;
+            IsMouseVisible = true;
+
+            plyBtn = new cButton(Content.Load<Texture2D>("Button"), graphics.GraphicsDevice);
+            plyBtn.setPosition(new Vector2(350, 300));
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+
+            MouseState mouse = Mouse.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            switch (CurrentState)
+            {
+                case GameState.MainMenu:
+                    if (plyBtn.isClicked == true) CurrentState = GameState.Playing;
+                    plyBtn.Update(mouse);
+                    break;
+                case GameState.Options:
+
+                    break;
+                case GameState.Playing:
+
+                    break;
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            switch (CurrentState)
+            {
+                case GameState.MainMenu:
+                    spriteBatch.Draw(Content.Load<Texture2D>("MainMenu"), new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
+                    plyBtn.Draw(spriteBatch);
+                    break;
+                case GameState.Options:
+
+                    break;
+                case GameState.Playing:
+
+                    break;
+            }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ FarmLand/FarmLand/cButton.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmLand
+{
+    class cButton
+    {
+        Texture2D Texture;
+        Vector2 Position;
+        Rectangle rectangle;
+
+        Color colour = new Color(255, 255, 255, 255);
+
+        public Vector2 size;
+
+        public cButton(Texture2D newTexture, GraphicsDevice graphics)
+        {
+            Texture = newTexture;
+
+            size = new Vector2(graphics.Viewport.Width / 8, graphics.Viewport.Height / 30);
+        }
+
+        bool down;
+        public bool isClicked;
+        public void Update(MouseState mouse)
+        {
+            rectangle = new Rectangle((int)Position.X, (int)Position.Y, (int)size.X, (int)size.Y);
+
+            Rectangle mouseRectangle = new Rectangle(mouse.X, mouse.Y, 1, 1);
+
+            if (mouseRectangle.Intersects(rectangle))
+            {
+                if (colour.A == 255) down = false;
+                if (colour.A == 0) down = true;
+                if (down) colour.A += 3; else colour.A -= 3;
+                if (mouse.LeftButton == ButtonState.Pressed) isClicked = true;
+            }
+            else if (colour.A < 255)
+            {
+                colour.A += 3;
+                isClicked = true;
+            }
+        }
+
+        public void setPosition(Vector2 newPosition)
+        {
+            Position = newPosition;
+        }
+
+        public void Draw (SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture,rectangle,colour);
+        }
+    }
+}
